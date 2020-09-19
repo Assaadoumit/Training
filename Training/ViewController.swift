6bd8f6e4f83df285df3bdb30eb05610c0b8d9889
Assaad doumit <assaad-doumit@hotmail.com>
2020-09-20 01:05:51 +0300

    Initial Commit

---
+++ Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Training
+//
+//  Created by Assaad Doumit on 9/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
