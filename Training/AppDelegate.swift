e1a1aabf686c3d29d60ac557b513ce0d5ec660f8
Assaad doumit <assaad-doumit@hotmail.com>
2020-10-05 02:16:04 +0300

    firebase push notification

--- Training/AppDelegate.swift
+++ Training/AppDelegate.swift
@@ -10,6 +10,7 @@ import Firebase
 import FirebaseAnalytics
 import FirebaseCore
 import FirebaseMessaging
+import UserNotifications
 
 
 @main class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -34,29 +35,15 @@ import FirebaseMessaging
         
         NotificationCenter.default.post(name: NSNotification.Name(rawValue: "ReceivedPushNotification"), object: userInfo)
         
-//        let vc = WebViewController()
-//        vc.openWeb(wurl: Link)
-//        self.window?.rootViewController?.present(vc, animated: true, completion: nil)
-//        func openNewLink(){
-//            let delegate = UIApplication.shared.delegate as! WebViewController
-//            let url = URL(string: "link")
-//            let request = URLRequest(url: url!)
-//            var urlRequest = URLRequest(url: url!)
-//            urlRequest.cachePolicy = .returnCacheDataDontLoad
-//            webView.loadRequest(request)
 //
-//        }
         completionHandler(UIBackgroundFetchResult.newData)
     }
     func messaging(_ messaging: Messaging, didReceiveremoteMessage: MessagingRemoteMessage) {
-        //         Convert to pretty-print JSON, just to show the message for testing
+        
         
     }
     
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Messaging.messaging().apnsToken = deviceToken
-        connectToFCM()
-    }
+//
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
@@ -80,27 +67,7 @@ import FirebaseMessaging
         
         return true
     }
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        connectToFCM()
-    }
-    func tokenRefreshNorification(notification : NSNotification){
-        let refreshedToken = InstanceID.instanceID().token(withAuthorizedEntity: <#T##String#>, scope: <#T##String#>, options: <#T##[AnyHashable : Any]?#>, handler: <#T##InstanceIDTokenHandler##InstanceIDTokenHandler##(String?, Error?) -> Void#>)
-        print("InstanceID Token: \(String(describing: refreshedToken))")
-        FirebaseApp.configure()
-        connectToFCM()
-    }
-    func connectToFCM(){
-        Messaging.messaging().shouldEstablishDirectChannel = true
-
-        Messaging.messaging().connect { (error) in
-            if (error != nil){
-                print("unable to connect to FCM \(error)")
-            }else{
-                print("Connected To FCM")
-            }
-        }
-    }
-    
+//
     // MARK: UISceneSession Lifecycle
     
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
